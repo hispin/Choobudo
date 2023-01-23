27e41dd1092a36944558a68683dba5d67f962ffc
Haggay <info@simplyhost.co.il>
2023-01-23 21:24:59 +0200

    Second version

--- app/src/main/java/com/generosity/choobudo/MainActivity.kt
+++ app/src/main/java/com/generosity/choobudo/MainActivity.kt
@@ -2,12 +2,13 @@ package com.generosity.choobudo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var b = 3
+        Log.d("","")
 
     }
 }
