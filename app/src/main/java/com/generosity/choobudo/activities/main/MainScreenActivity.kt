3cfba7b1e1cdef6a6db7f128ac0064bbde2b1eef
Haggay <info@simplyhost.co.il>
2023-04-14 01:41:11 +0300

    design main screen

---
+++ app/src/main/java/com/generosity/choobudo/activities/main/MainScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.generosity.choobudo.activities.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.generosity.choobudo.R
+
+class MainScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+    }
+}
